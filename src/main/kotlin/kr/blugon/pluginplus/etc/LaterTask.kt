058af420647d78a587e21691b8ae246dccc8f44a
blugon0921 <blugon0921@gmail.com>
2024-02-17 12:16:39 +0900

    ✨ 1.1.6

--- src/main/kotlin/kr/blugon/pluginplus/etc/LaterTask.kt
+++ src/main/kotlin/kr/blugon/pluginplus/etc/LaterTask.kt
@@ -3,7 +3,7 @@ package kr.blugon.pluginplus.etc
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 
-class LaterTask(val plugin: JavaPlugin, val delay : Long, val task: () -> Unit, run : Boolean = true) {
+class LaterTask(val plugin: JavaPlugin, val delay : Long, val run : Boolean = true, val task: () -> Unit) {
 
     init {
         if (run) {
--- src/main/kotlin/kr/blugon/pluginplus/etc/TimerTask.kt
+++ src/main/kotlin/kr/blugon/pluginplus/etc/TimerTask.kt
@@ -3,7 +3,7 @@ package kr.blugon.pluginplus.etc
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 
-class TimerTask(val plugin: JavaPlugin, val delay : Long, val period: Long, val task: () -> Unit, run : Boolean = true) {
+class TimerTask(val plugin: JavaPlugin, val delay : Long, val period: Long, val run : Boolean = true, val task: () -> Unit) {
 
     init {
         if(run) {
